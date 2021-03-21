a7cfefe570d68cddacd957b8b027f6687fc8f904
Yoshiyuki Hirano <yhirano@me.com>
2021-03-21 17:59:59 +0900

    Configure polyfills

---
+++ app/javascript/packs/polyfill.js
@@ -0,0 +1,7 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "whatwg-fetch";
+import "@ungap/custom-elements";
+import "custom-event-polyfill";
+import "abortcontroller-polyfill/dist/polyfill-patch-fetch";
+import "@stimulus/polyfills";
