824687a109c9a5baa15beb41882665661d185a72
Yoshiyuki Hirano <yhirano@me.com>
2021-03-21 18:04:39 +0900

    Setup Turbo.visit on views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,13 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
 ActiveStorage.start()
+
+window.Turbo = Turbo;
 
 import "controllers"
